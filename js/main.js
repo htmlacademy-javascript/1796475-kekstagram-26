ccc12fe12341882986bf208b9bb1a7d25edc85b2
Sergey <sergey.svetlov.91@list.ru>
2022-06-08 12:31:49 +0300

    функции

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+function getRandomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let randomNumber;
+  if (min > max) {
+    randomNumber = max + Math.random() * (min + 1 - max);
+  } else {
+    randomNumber = min + Math.random() * (max + 1 - min);
+  }
+  return Math.floor(randomNumber);
+}
 
+//Функция для проверки максимальной длины строки
+function checkLength(inputString, maxLength) {
+  return inputString.length <= maxLength;
+}
+
+getRandomInteger(0, 10);
+checkLength('город', 10);
