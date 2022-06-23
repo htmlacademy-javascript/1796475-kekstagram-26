7f41d5381bb198591c16e850a5f0b4f5f21886ca
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-06-24 02:19:16 +0300

    Модуляция

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomInteger(min, max) {
-  // случайное число от min до (max+1)
-  let randomNumber;
-  if (min > max) {
-    randomNumber = max + Math.random() * (min + 1 - max);
-  } else {
-    randomNumber = min + Math.random() * (max + 1 - min);
-  }
-  return Math.floor(randomNumber);
-}
+import {createPhotoDescriptions} from './data.js';
 
-//Функция для проверки максимальной длины строки
-function checkLength(inputString, maxLength) {
-  return inputString.length <= maxLength;
-}
-
-getRandomInteger(0, 10);
-checkLength('город', 10);
+createPhotoDescriptions();
