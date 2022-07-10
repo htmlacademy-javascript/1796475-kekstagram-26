5150398846cac2a051f02ea759e5b63db9993fa4
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-07-11 00:42:15 +0300

    комментарии

--- js/big_picture.js
+++ js/big_picture.js
@@ -2,6 +2,7 @@ import {isEscapeKey} from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+const similarCommentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
 const onBigPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -10,10 +11,20 @@ const onBigPictureEscKeydown = (evt) => {
   }
 };
 
-const showBigPicture = ({url, likes, comments}) => {
+const renderComment = ({avatar, message, name}) => {
+  const commentElement = similarCommentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = avatar;
+  commentElement.querySelector('.social__picture').alt = name;
+  commentElement.querySelector('.social__text').textContent = message;
+  return commentElement;
+};
+
+const showBigPicture = ({url, likes, comments, description}) => {
   bigPicture.querySelector('.big-picture__img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.comments-count').textContent = comments.length;
+  document.querySelector('.social__comments').append(...comments.map(renderComment));
+  bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.classList.remove('hidden');
   document.querySelector('.social__comment-count').classList.add('hidden');
   document.querySelector('.comments-loader').classList.add('hidden');
@@ -21,8 +32,11 @@ const showBigPicture = ({url, likes, comments}) => {
   document.addEventListener('keydown', onBigPictureEscKeydown);
 };
 
-bigPictureCancel.addEventListener('click', () => {
+const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
-});
+  document.removeEventListener('keydown', onBigPictureEscKeydown);
+};
 
-export{showBigPicture};
+bigPictureCancel.addEventListener('click', closeBigPicture);
+
+export{showBigPicture, closeBigPicture};
--- js/render.js
+++ js/render.js
@@ -1,16 +1,16 @@
-import {showBigPicture} from './big_picture.js';
+import {showBigPicture, closeBigPicture} from './big_picture.js';
 
 const similarListElement = document.querySelector('.pictures');
 const similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderPhoto = ({url, likes, comments}) => {
+const renderPhoto = ({url, likes, comments, description}) => {
 
   const photoElement = similarPictureTemplate.cloneNode(true);
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
 
-  photoElement.addEventListener('click', () => showBigPicture({url, likes, comments}));
+  photoElement.addEventListener('click', () => showBigPicture({url, likes, comments, description}));
 
   return photoElement;
 };
@@ -18,5 +18,7 @@ const renderPhoto = ({url, likes, comments}) => {
 const renderPhotos = (photos) => {
   similarListElement.append(...photos.map(renderPhoto));
 };
+
+closeBigPicture();
 
 export {renderPhotos};
