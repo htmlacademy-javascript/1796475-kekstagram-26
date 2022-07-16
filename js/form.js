da842d5bc619f0c01f9c8d50889f2019576a3580
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-07-16 13:59:02 +0300

    правки

--- js/form.js
+++ js/form.js
@@ -8,8 +8,8 @@ const uploadCancel = imgUploadOverlay.querySelector('#upload-cancel');
 const textHashtags = imgUploadOverlay.querySelector('.text__hashtags');
 const textDescription = imgUploadOverlay.querySelector('.text__description');
 const imgUploadSubmit = imgUploadOverlay.querySelector('.img-upload__submit');
-
-const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const maxHashtagsNumber = 5;
+const hashTagRegexp = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
 const onLoadingEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -23,7 +23,6 @@ const loading = () => {
   body.classList.add('modal-open');
   document.addEventListener('keydown', onLoadingEscKeydown);
 };
-uploadFile.addEventListener('change', loading);
 
 const closeLoading = () => {
   imgUploadOverlay.classList.add('hidden');
@@ -31,24 +30,30 @@ const closeLoading = () => {
   uploadFile.removeEventListener('change', loading);
 };
 
-uploadCancel.addEventListener('click', closeLoading);
+const hashtagsArray = () => textHashtags.value.trim().toLowerCase().split(/\s+/);
 
-const pristine = new Pristine(imgUploadForm);
+const isCorrectHashtag = (element) => hashTagRegexp.test(element);
 
-const validateHashtags = () => {
-  const hashtagsArray = textHashtags.value.trim().toLowerCase().split(/\s+/);
-  hashtagsArray.length <= 5 && hashtagsArray.every((element) => element !== re );
-};
+const isCorrectHashtagsNumber = () => hashtagsArray().length <= maxHashtagsNumber;
 
-pristine.addValidator(textHashtags, validateHashtags, 'некорректный набор хэштегов');
+const validateHashtags = () => hashtagsArray().every(isCorrectHashtag);
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo:'img-upload__field-wrapper',
+  errorTextParent:'img-upload__field-wrapper',
+});
+
+pristine.addValidator(textHashtags, validateHashtags, 'строка после решётки не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации, эмодзи');
+pristine.addValidator(textHashtags, isCorrectHashtagsNumber, `максимальное количество хэштегов - ${maxHashtagsNumber}`);
 
 imgUploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
-  if (isValid) {
-    console.log('Можно отправлять');
-  } else {
-    console.log('Форма невалидна');
+  if (!isValid) {
+    evt.preventDefault();
   }
 });
+
+uploadFile.addEventListener('change', loading);
+uploadCancel.addEventListener('click', closeLoading);
 
