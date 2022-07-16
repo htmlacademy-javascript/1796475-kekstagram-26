7459d5202552f9a1138cce64e71e9d40d26e3b90
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-07-16 21:08:47 +0300

    Правда или действие (часть 2)

--- js/big_picture.js
+++ js/big_picture.js
@@ -3,6 +3,9 @@ import {isEscapeKey} from './util.js';
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
 const similarCommentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const visibleCommentsCount = bigPicture.querySelector('.visible-comments-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 
 const onBigPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -19,19 +22,39 @@ const renderComment = ({avatar, message, name}) => {
   return commentElement;
 };
 
+const hideComments = () => {
+  bigPicture.querySelectorAll('.social__comment').forEach((element, i) => {
+    if (i>4) {
+      element.classList.add('hidden');
+    }
+  });
+};
+
+const uploadMoreComments = () => {
+  bigPicture.querySelectorAll('.social__comment.hidden').forEach((element, i) => {
+    if (i<5) {
+      element.classList.remove('hidden');
+    }
+  });
+};
+
 const showBigPicture = ({url, likes, comments, description}) => {
   bigPicture.querySelector('.social__comments').textContent = '';
   bigPicture.querySelector('.big-picture__img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  commentsCount.textContent = comments.length;
   bigPicture.querySelector('.social__comments').append(...comments.map(renderComment));
   bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.classList.remove('hidden');
-  document.querySelector('.social__comment-count').classList.add('hidden');
-  document.querySelector('.comments-loader').classList.add('hidden');
+  //document.querySelector('.social__comment-count').classList.add('hidden');
+  //document.querySelector('.comments-loader').classList.add('hidden');
+  hideComments();
+  visibleCommentsCount.textContent = comments.length - bigPicture.querySelectorAll('.social__comment.hidden').length;
+  commentsLoader.addEventListener('click', uploadMoreComments);
   document.querySelector('body').classList.add('modal-open');
   document.addEventListener('keydown', onBigPictureEscKeydown);
 };
+
 
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
