d8acfd5c10c7e144ed83120d9df587bcc0f83b0b
SergeySvetlov <82765440+SergeySvetlov@users.noreply.github.com>
2022-07-24 22:31:32 +0300

    работа с сервером

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 import {createPhotoDescriptions} from './data.js';
+import {getData} from './api.js';
+import {showAlert} from './util.js';
 import {renderPhotos} from './render.js';
 import './form.js';
 import './editor.js';
 
-const photoDescriptions = createPhotoDescriptions();
+const onGetDataSuccess = (data) => {
+  renderPhotos(data);
+};
 
+getData(onGetDataSuccess, showAlert);
+/*
+const photoDescriptions = createPhotoDescriptions();
 renderPhotos(photoDescriptions);
+*/
